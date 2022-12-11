c1071d5e3d6e6866cfeb4e903ee6dbbc8027d44f
loisuzoma <uzomalois2@gmail.com>
2022-12-11 12:29:28 -0800

    Patience persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible number seperated by comma
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
