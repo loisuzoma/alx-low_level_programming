25470be3d298150dc1af0e39235cacc316938460
loisuzoma <uzomalois2@gmail.com>
2022-12-11 12:15:12 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
