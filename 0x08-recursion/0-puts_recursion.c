2c55dc004f830374e5db9d44c4e8057b9f7de188
loisuzoma <uzomalois2@gmail.com>
2023-01-05 19:59:12 -0800

    She locked away a secret deep inside her

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#includee "main.h"
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
@@ -13,7 +13,6 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
 	else
-		_putchar('\n')
+		_putchar('\n');
 }
