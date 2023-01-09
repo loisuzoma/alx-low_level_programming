f2258c7b0fd9a889a7880c247925509c48071885
loisuzoma <uzomalois2@gmail.com>
2023-01-09 08:07:53 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ *
+ *  @num: int to be checked
+ *
+ *  Return: 1 if num is a digit, o otherwise.
+ *
+ *
+ *
+ *
+ */
+
+
+
+int _isdigit(int num)
+
+
+
+{
+
+	return (num >= '0' && num <= '9');
+
+
+
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+
+/**
+ * _isalpha - a function that checks for alphabetic character
+ *
+ * @c: single letter input
+ *
+ * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
+ *
+ *
+ *
+ */
+
+
+
+int_isalpha(int c)
+
+
+
+{
+
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+
+		return (1);
+
+	else
+
+		return (0);
+
+}
