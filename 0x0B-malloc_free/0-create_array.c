43a6c8695f224a7bd0836259cb7825b1c0170a0d
loisuzoma <uzomalois2@gmail.com>
2023-01-09 20:10:33 -0800

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * *create_array - creates an array of chars,
+ * create_array - creates an array of chars,
  * and initializes it with a specific char
  * @size: size of the array to create
  * @c: char to initialize the array c
@@ -28,3 +28,4 @@ char *create_array(unsigned int size, char c)
 
 		return (p);
 	}
+}
